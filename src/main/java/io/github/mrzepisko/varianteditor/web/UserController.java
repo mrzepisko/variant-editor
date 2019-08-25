c240ec8f82319e9d7c0d10021e9508ba447b209a
mrzepisko <rz.marcin@gmail.com>
2019-08-25 23:38:35 +0200

    password validation

--- src/main/java/io/github/mrzepisko/varianteditor/web/UserController.java
+++ src/main/java/io/github/mrzepisko/varianteditor/web/UserController.java
@@ -3,11 +3,15 @@ package io.github.mrzepisko.varianteditor.web;
 import io.github.mrzepisko.varianteditor.model.User;
 import io.github.mrzepisko.varianteditor.service.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @RestController
 public class UserController {
@@ -20,7 +24,7 @@ public class UserController {
 
     @PostMapping("/users")
     @ResponseBody
-    public User newUser(@NotNull @NotEmpty String password) {
+    public User newUser(@RequestParam @Pattern(regexp = ".{8,}(?!\\d+)") String password) {
         return userService.register(password);
     }
 }

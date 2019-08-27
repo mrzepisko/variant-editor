6afe6fcbcbe8bdc27091fc53877195bce199bcd3
mrzepisko <rz.marcin@gmail.com>
2019-08-27 20:56:42 +0200

    unused imports

--- src/test/java/io/github/mrzepisko/varianteditor/web/UserControllerTest.java
+++ src/test/java/io/github/mrzepisko/varianteditor/web/UserControllerTest.java
@@ -6,8 +6,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(UserController.class)
